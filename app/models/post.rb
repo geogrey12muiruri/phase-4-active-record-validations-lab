0fb59c6d12fdb6f9a0a9e29fe7137dba7d139d58
geogrey12muiruri <greggambugua@gmail.com>
2022-11-25 09:14:13 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    #Post summary is a maximum of 250 characters
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait
+   
+    def is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Title is Invalid")
+        end
+    end
+    
+    #Custom validation
+    
 end
